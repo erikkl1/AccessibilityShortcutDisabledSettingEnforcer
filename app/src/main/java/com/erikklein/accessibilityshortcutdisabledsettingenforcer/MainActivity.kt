443f58378062d2eb6673530758c977b9878eb5dd
unknown <nothing@nothing.de>
2025-03-20 12:28:24 +0100

    optimize imports

--- app/src/main/java/com/erikklein/accessibilityshortcutdisabledsettingenforcer/MainActivity.kt
+++ app/src/main/java/com/erikklein/accessibilityshortcutdisabledsettingenforcer/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.erikklein.accessibilityshortcutdisabledsettingenforcer
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
